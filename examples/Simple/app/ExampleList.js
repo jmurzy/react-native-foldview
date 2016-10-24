2a2effd7fd21b5f3bb3cd710ce25543e43780de2
Jake Murzy <jake@murzy.com>
2016-10-23 22:18:09 -0700

    prettify example app

--- examples/Simple/app/ExampleList.js
+++ examples/Simple/app/ExampleList.js
@@ -1,27 +1,41 @@
 import React from 'react';
 
 import {
+  Platform,
   ScrollView,
+  StatusBar,
   StyleSheet,
+  View,
 } from 'react-native';
 
 import Row from './Row';
 
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
+
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  scrollView: {
     backgroundColor: '#4A637D',
     flex: 1,
     padding: 10,
+    paddingTop: STATUSBAR_HEIGHT,
   },
 });
 
 export default () => (
-  <ScrollView
-    style={styles.container}
-  >
-    <Row zIndex={100} />
-    <Row zIndex={90} />
-    <Row zIndex={80} />
-    <Row zIndex={70} />
-  </ScrollView>
+  <View style={styles.container}>
+    <StatusBar
+      barStyle="light-content"
+    />
+    <ScrollView
+      style={styles.scrollView}
+    >
+      <Row zIndex={100} />
+      <Row zIndex={90} />
+      <Row zIndex={80} />
+      <Row zIndex={70} />
+    </ScrollView>
+  </View>
 );
